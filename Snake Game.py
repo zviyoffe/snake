c18efe3589f5360d03b35c0332e2507d7644e27e
Avraham Yoffe <avraham.yoffe@weka.io>
2020-07-20 23:16:16 +0300

    david + zvi + aba (music)

--- Snake Game.py
+++ Snake Game.py
@@ -1,7 +1,7 @@
 """
   יבצה שחנ
 Snake Eater
-Made with PyGame
+Made with PyGaime
 """
 
 import pygame, sys, time, random
@@ -13,11 +13,14 @@ import pygame, sys, time, random
 # Hard      ->  40
 # Harder    ->  60
 # Impossible->  120
-difficulty = 25
+
+DIFFICULTY = 25
+difficulty = DIFFICULTY
 
 # Window size
 frame_size_x = 720
 frame_size_y = 480
+
 
 # Checks for errors encountered
 check_errors = pygame.init()
@@ -29,10 +32,17 @@ if check_errors[1] > 0:
 else:
     print('[+] Game successfully initialised')
 
+pygame.init()
 
+#info = pygame.display.Info()
+#print(info)
+
+
+#frame_size_y = info.current_h
+#frame_size_x = info.current_w
 # Initialise game window
-pygame.display.set_caption('Snake Eater')
-game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
+pygame.display.set_caption('נחש הצבי')
+game_window = pygame.display.set_mode((frame_size_x, frame_size_y), pygame.RESIZABLE | pygame.DOUBLEBUF)
 
 
 # Colors (R, G, B)
@@ -50,28 +60,32 @@ fps_controller = pygame.time.Clock()
 # Game variables
 snake_pos = None
 snake_body = None
+score = 0
 
 def init():
     global snake_pos
     global snake_body
+    global score
+    global difficulty
+    score = 0
+    difficulty = DIFFICULTY
     snake_pos = [100, 50]
     snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
 
 init()
 
-food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+random.seed(21)
+food_pos = [[random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10] for i in range(33)]
 food_spawn = True
 
 direction = 'RIGHT'
 change_to = direction
 
-score = 0
-
 
 # Game Over
 def game_over():
     my_font = pygame.font.SysFont('times new roman', 90)
-    game_over_surface = my_font.render('YOU STILL ALIVE!', True, red)
+    game_over_surface = my_font.render('YOU STILL ALIVE!', True, green )
     game_over_rect = game_over_surface.get_rect()
     game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
     game_window.fill(black)
@@ -101,10 +115,28 @@ def show_score(choice, color, font, size):
 _sleep = 0
 _change_to_save = 'RIGHT'
 
+_events = []
+
+tapS = pygame.mixer.Sound("tapuah.aiff")
+scrS = pygame.mixer.Sound("titss.wav")
+music = pygame.mixer.music.load("music.mp3")
+
+pygame.mixer.music.play(-1) 
+
 # Main logic
 while True:
-    import time
+    #info = pygame.display.Info()
+    #print(info)
+    #frame_size_y = info.current_h
+    #frame_size_x = info.current_w
+    # frame_size_x, frame_size_y = game_window.get_size()
     for event in pygame.event.get():
+        if event.type == pygame.VIDEORESIZE:
+            # There's some code to add back window content here.
+            game_window = pygame.display.set_mode((event.w, event.h),
+                                              pygame.RESIZABLE)
+            frame_size_y, frame_size_x = event.h, event.w
+
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
@@ -123,21 +155,9 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event(pygame.QUIT))
             if event.key == ord('b'):
+                print("'b' pressed")
                 _change_to_save = change_to
                 change_to = 'BREAK'
-
-            if event.key == ord('+'):
-                _sleep = _sleep + 0.05
-   
-            if event.key == ord('-'):
-                _sleep = _sleep - 0.05
-                
-                
-            if _sleep < 0:
-                _sleep = 0
-            if _sleep > 5:
-                _sleep = 5
-   
 
    # Making sure the snake cannot move in the opposite direction instantaneously
     if change_to == 'UP' and direction != 'DOWN':
@@ -166,17 +186,27 @@ while True:
 
     # Snake body growing mechanism
     snake_body.insert(0, list(snake_pos))
-    if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
+    i = -1
+    _eat = False
+    for _food in food_pos:
+        i += 1
+        if snake_pos[0] == _food[0] and snake_pos[1] == _food[1]:
+            _eat = True
+            break
+
+    if _eat:
         score += 1
-        food_spawn = False
+        if score % 10 == 0:
+            difficulty += 5
+            scrS.play()
+        else:
+            tapS.play()
+
+        food_pos.pop(i)
+        food_pos.append([random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10])
     else:
         snake_body.pop()
-
-    # Spawning food on the screen
-    if not food_spawn:
-        food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
-    food_spawn = True
-
+    
     # GFX
     game_window.fill(black)
     for pos in snake_body:
@@ -185,20 +215,22 @@ while True:
         # xy-coordinate -> .Rect(x, y, size_x, size_y)
         pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
 
+
     # Snake food
-    pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+    for _food in food_pos:
+        pygame.draw.rect(game_window, white, pygame.Rect(_food[0], _food[1], 10, 10))
 
     # Game Over conditions
     # Getting out of bounds
     if snake_pos[0] < 0:
         snake_pos[0] = frame_size_x-10
-    
+
     if snake_pos[0] > frame_size_x-10:
         snake_pos[0] = 0
-    
+
     if snake_pos[1] > frame_size_y-10:
         snake_pos[1] = 0
-    if snake_pos[1] <= 0:
+    elif snake_pos[1] <= 0:
         snake_pos[1] = frame_size_y-10
 
 #    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
@@ -213,4 +245,3 @@ while True:
     pygame.display.update()
     # Refresh rate
     fps_controller.tick(difficulty)
-    time.sleep(_sleep)
